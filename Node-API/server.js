3b8870cf3c9cfe22e199d5a5769a22820a508bfe
ArthurViniDev <kenzygamedev@gmail.com>
2025-05-12 23:38:39 -0300

    mongodb

--- Node-API/server.js
+++ Node-API/server.js
@@ -1,20 +1,32 @@
 import express from 'express';
+import pkg from '@prisma/client';
+
+const { PrismaClient } = pkg;
+const prisma = new PrismaClient();
 
 const app = express();
 app.use(express.json());
 
 const users = [];
 
-app.post('/usuarios', (request, response) => {
+app.post('/usuarios', async (request, response) => {
     console.log(request.body);
 
-    users.push(request.body);
+    await prisma.user.create({
+        data: {
+            email: request.body.email,
+            name: request.body.name,
+            age: request.body.age,
+        }
+    });
 
     response.status(201).json(request.body);
 });
 
-// TODO: rota para listar usuários
 app.get('/usuarios', (request, response) => {
     response.status(200).json(users);
 });
-app.listen(3000);
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});

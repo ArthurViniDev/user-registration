b247b6ab0c3857ad70015267a9cd598cdc35e5c0
ArthurViniDev <kenzygamedev@gmail.com>
2025-05-13 00:16:21 -0300

    delete working

--- Node-API/server.js
+++ Node-API/server.js
@@ -1,5 +1,5 @@
-import express from 'express';
-import pkg from '@prisma/client';
+import express from "express";
+import pkg from "@prisma/client";
 
 const { PrismaClient } = pkg;
 const prisma = new PrismaClient();
@@ -7,40 +7,49 @@ const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
 
-app.post('/usuarios', async (request, response) => {
-    console.log(request.body);
+app.post("/usuarios", async (request, response) => {
+  console.log(request.body);
 
-    await prisma.user.create({
-        data: {
-            email: request.body.email,
-            name: request.body.name,
-            age: request.body.age,
-        }
-    });
-    response.status(201).json(request.body);
+  await prisma.user.create({
+    data: {
+      email: request.body.email,
+      name: request.body.name,
+      age: request.body.age,
+    },
+  });
+  response.status(201).json(request.body);
 });
 
-app.put('/usuarios/:id', async (request, response) => {
-    console.log(request);
+app.put("/usuarios/:id", async (request, response) => {
+  console.log(request);
 
-    await prisma.user.update({
-        where: {
-            id: request.params.id
-        },
-        data: {
-            email: request.body.email,
-            name: request.body.name,
-            age: request.body.age,
-        }
-    });
-    response.status(201).json(request.body);
+  await prisma.user.update({
+    where: {
+      id: request.params.id,
+    },
+    data: {
+      email: request.body.email,
+      name: request.body.name,
+      age: request.body.age,
+    },
+  });
+  response.status(201).json(request.body);
 });
 
-app.get('/usuarios', async (request, response) => {
-    const users = await prisma.user.findMany()
-    response.status(200).json(users);
+app.get("/usuarios", async (request, response) => {
+  const users = await prisma.user.findMany();
+  response.status(200).json(users);
+});
+
+app.delete("/usuarios/:id", async (request, response) => {
+  await prisma.user.delete({
+    where: {
+      id: request.params.id,
+    },
+  });
+  response.status(200).json({ message: "Usuário deletado com sucesso" });
 });
 
 app.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000');
+  console.log("Servidor rodando na porta 3000");
 });
